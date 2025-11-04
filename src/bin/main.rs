2270705c6214a5c134ffadb5d3a05490afacc625
IznaDev <oussama.boukhrass@gmail.com>
2025-11-04 16:47:34 +0100

    adding drop trait implementation, Message enum & some tests,

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,27 +1,26 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
-use std::thread;
-
 use web_server_multi_threaded::ThreadPool;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     let pool = ThreadPool::new(5);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(3) {
         let stream = stream.unwrap();
         pool.execute(move || {
             handle_connection(stream);
         });
     }
+    println!("Shutting down.");
 }
 
 // Function to handle incoming TCP connections
 pub fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0u8; 512];
     stream.read(&mut buffer).unwrap(); // move the data from the stream into the buffer
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..])); // each non-utf8 character is replaced with � thanks to from_utf8_lossy
+    //println!("Request: {}", String::from_utf8_lossy(&buffer[..])); // each non-utf8 character is replaced with � thanks to from_utf8_lossy
 
     let get = b"GET / HTTP/1.1\r\n";
     //compare the request in the buffer to the expected GET request
--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,22 @@ use std::thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
+// Implementation of ThreadPool. It creates a new thread pool and assigns jobs to workers.
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
@@ -31,19 +37,65 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
+// Implementation of Worker. Each worker waits for messages and executes jobs.
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || {
             loop {
-                let job = receiver.lock().unwrap().recv().unwrap();
-                println!("Worker {} got a job; executing.", id);
-                job();
+                let message = receiver.lock().unwrap().recv().unwrap();
+                match message {
+                    Message::NewJob(job) => {
+                        println!("Worker {} got a job; executing.", id);
+                        job();
+                    }
+                    Message::Terminate => {
+                        println!("Worker {} was told to terminate.", id);
+                        break;
+                    }
+                }
             }
         });
-        Worker { id, thread }
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
+
+// Implementation of Drop trait for ThreadPool to gracefully shut down all workers.
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        for worker in &mut self.workers {
+            println!("Shutting down Worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_thread_pool_creation() {
+        let pool = ThreadPool::new(4);
+        assert_eq!(pool.workers.len(), 4);
+    }
+    #[test]
+    #[should_panic(expected = "size > 0")]
+    fn test_thread_pool_creation_zero_size() {
+        ThreadPool::new(0);
     }
 }
